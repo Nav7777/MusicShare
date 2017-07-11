72f5dc7e386e20a4351716752450562fd6544bb1
KRISHNAIOS <akrish.valentine@gmail.com>
2017-07-11 23:33:52 +0530

    added urlrequest cache

--- MusicApp/MASongListCellTableViewCell.m
+++ MusicApp/MASongListCellTableViewCell.m
@@ -26,7 +26,7 @@
     self.lblSongName.text=songObject.albumName;
     self.lblArtistname.text=songObject.artist;
     self.lblArtistAlbum.text=songObject.albumName;
-    [[[NSURLSession sharedSession] dataTaskWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:songObject.albumArt]] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+    [[[NSURLSession sharedSession] dataTaskWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:songObject.albumArt] cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:60.0f] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         dispatch_async(dispatch_get_main_queue(), ^{
             self.imgArtistArt.image = [UIImage imageWithData:data];
         });
